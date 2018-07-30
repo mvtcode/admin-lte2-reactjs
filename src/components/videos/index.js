08589f79b08a2893b807864be281395f7e9a81e8
tanmv <macvantan@hotmail.com.vn>
2018-07-31 01:35:25 +0700

    paging

--- src/components/videos/index.js
+++ src/components/videos/index.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import Pagination from "react-js-pagination";
 import {pageType} from "../../actions";
 import stores from '../../stores';
 
 class Videos extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handlePageChanged = this.handlePageChanged.bind(this);
+
+		this.state = {
+			page: {
+				total: 450,
+				page_size: 20,
+				page_index: 1,
+				visiblePage: 5
+			}
+		};
+	}
+
 	componentWillMount() {
 		stores.dispatch({
 			type: pageType.SET_PAGE,
@@ -14,11 +30,91 @@ class Videos extends Component {
 		});
 	}
 
+	handlePageChanged(newPage) {
+		const state = this.state;
+		state.page.page_index = newPage;
+		this.setState(state);
+	}
+
 	render() {
 		return (
 			<React.Fragment>
-				<div style={{ textAlign: "center",marginTop:'10rem' }}>
-					<h1>Videos is there</h1>
+				<div className="row">
+					<div className="col-xs-12">
+						<div className="box">
+							<div className="box-header">
+								<h3 className="box-title">List videos</h3>
+							</div>
+							<div className="box-body">
+								<div className="dataTables_wrapper form-inline dt-bootstrap">
+									<div className="row">
+										<div className="col-sm-12">
+											<table className="table table-bordered table-striped">
+												<thead>
+												<tr>
+													<th>Rendering engine</th>
+													<th>Browser</th>
+													<th>Platform(s)</th>
+													<th>Engine version</th>
+													<th>CSS grade</th>
+												</tr>
+												</thead>
+												<tbody>
+												<tr>
+													<td>Trident</td>
+													<td>Internet
+														Explorer 4.0
+													</td>
+													<td>Win 95+</td>
+													<td> 4</td>
+													<td>X</td>
+												</tr>
+												<tr>
+													<td>Trident</td>
+													<td>Internet
+														Explorer 5.0
+													</td>
+													<td>Win 95+</td>
+													<td>5</td>
+													<td>C</td>
+												</tr>
+												</tbody>
+												<tfoot>
+												<tr>
+													<th>Rendering engine</th>
+													<th>Browser</th>
+													<th>Platform(s)</th>
+													<th>Engine version</th>
+													<th>CSS grade</th>
+												</tr>
+												</tfoot>
+											</table>
+										</div>
+									</div>
+
+									<div className="row">
+										<div className="col-sm-5">
+											<div className="dataTables_info">Showing 1 to 10 of 57 entries</div>
+										</div>
+										<div className="col-sm-7">
+											<Pagination
+												totalItemsCount={this.state.page.total}
+												itemsCountPerPage={this.state.page.page_size}
+												activePage={this.state.page.page_index}
+												pageRangeDisplayed={this.state.page.visiblePage}
+												onChange={this.handlePageChanged}
+												firstPageText={<i className='fa fa-fast-backward'/>}
+												lastPageText={<i className='fa fa-fast-forward'/>}
+												prevPageText={<i className='fa fa-step-backward'/>}
+												nextPageText={<i className='fa fa-step-forward'/>}
+											/>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</React.Fragment>
 		);
--- src/partials/Header.js
+++ src/partials/Header.js
@@ -17,145 +17,141 @@ class Header extends Component {
 		this.setState(state);
 	}
 
-	progress_bar = {
-		width: "20%"
-	};
+	render() {
+		return (
+			<header className="main-header">
+				<NavLink to="/" className="logo">
+					<span className="logo-mini">VAFA</span>
+					<span className="logo-lg"><b>VAFA</b> Videos</span>
+				</NavLink>
 
-  render() {
-    return (
-      <header className="main-header">
-        <NavLink to="/" className="logo">
-          <span className="logo-mini">VAFA</span>
-          <span className="logo-lg"><b>VAFA</b> Videos</span>
-        </NavLink>
+				<nav className="navbar navbar-static-top" role="navigation">
+					<NavLink to="/" className="sidebar-toggle" data-toggle="push-menu" role="button">
+						<span className="sr-only">Toggle navigation</span>
+					</NavLink>
+					<div className="navbar-custom-menu">
+						<ul className="nav navbar-nav">
+							{/*<li className="dropdown messages-menu">
+								<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+									<i className="fa fa-envelope-o"></i>
+									<span className="label label-success">4</span>
+								</a>
+								<ul className="dropdown-menu">
+									<li className="header">You have 4 messages</li>
+									<li>
+										<ul className="menu">
+											<li>
+												<a href="#">
+													<div className="pull-left">
+														<img src="dist/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
+													</div>
+													<h4>
+														Support Team
+														<small><i className="fa fa-clock-o"></i> 5 mins</small>
+													</h4>
+													<p>Why not buy a new awesome theme?</p>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="footer"><a href="#">See All Messages</a></li>
+								</ul>
+							</li>
 
-        <nav className="navbar navbar-static-top" role="navigation">
-          <NavLink to="/" className="sidebar-toggle" data-toggle="push-menu" role="button">
-            <span className="sr-only">Toggle navigation</span>
-          </NavLink>
-          <div className="navbar-custom-menu">
-            <ul className="nav navbar-nav">
-	            {/*<li className="dropdown messages-menu">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                  <i className="fa fa-envelope-o"></i>
-                  <span className="label label-success">4</span>
-                </a>
-                <ul className="dropdown-menu">
-                  <li className="header">You have 4 messages</li>
-                  <li>
-                    <ul className="menu">
-                      <li>
-                        <a href="#">
-                          <div className="pull-left">
-                            <img src="dist/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
-                          </div>
-                          <h4>
-                            Support Team
-                            <small><i className="fa fa-clock-o"></i> 5 mins</small>
-                          </h4>
-                          <p>Why not buy a new awesome theme?</p>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="footer"><a href="#">See All Messages</a></li>
-                </ul>
-              </li>
-
-              <li className="dropdown notifications-menu">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                  <i className="fa fa-bell-o"></i>
-                  <span className="label label-warning">10</span>
-                </a>
-                <ul className="dropdown-menu">
-                  <li className="header">You have 10 notifications</li>
-                  <li>
-                    <ul className="menu">
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-users text-aqua"></i> 5 new members joined today
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="footer"><a href="#">View all</a></li>
-                </ul>
-              </li>
-              <li className="dropdown tasks-menu">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                  <i className="fa fa-flag-o"></i>
-                  <span className="label label-danger">9</span>
-                </a>
-                <ul className="dropdown-menu">
-                  <li className="header">You have 9 tasks</li>
-                  <li>
-                    <ul className="menu">
-                      <li>
-                        <a href="#">
-                          <h3>
-                            Design some buttons
-                            <small className="pull-right">20%</small>
-                          </h3>
-                          <div className="progress xs">
-                            <div className="progress-bar progress-bar-aqua" style={this.progress_bar} role="progressbar"
-                                 aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                              <span className="sr-only">20% Complete</span>
-                            </div>
-                          </div>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="footer">
-                    <a href="#">View all tasks</a>
-                  </li>
-                </ul>
-              </li>*/}
-              <li className="dropdown user user-menu">
-                <NavLink to="/" className="dropdown-toggle" data-toggle="dropdown">
-                  <img src="/dist/img/avatar.jpg" className="user-image" alt="avatar"/>
-	                <span className="hidden-xs">{this.state.user.email}</span>
-                </NavLink>
-                <ul className="dropdown-menu">
-                  <li className="user-header">
-                    <img src="/dist/img/avatar.jpg" className="img-circle" alt="avatar"/>
-                      <p>
-                        Alexander Pierce - Web Developer
-                        <small>Member since Nov. 2012</small>
-                      </p>
-                  </li>
-                  <li className="user-body">
-                    <div className="row">
-                      <div className="col-xs-4 text-center">
-                        <NavLink to="/">Followers</NavLink>
-                      </div>
-                      <div className="col-xs-4 text-center">
-                        <NavLink to="/">Sales</NavLink>
-                      </div>
-                      <div className="col-xs-4 text-center">
-                        <NavLink to="/">Friends</NavLink>
-                      </div>
-                    </div>
-                  </li>
-                  <li className="user-footer">
-                    <div className="pull-left">
-                      <NavLink to="/profile" className="btn btn-default btn-flat">Profile</NavLink>
-                    </div>
-                    <div className="pull-right">
-                      <NavLink to="/logout" className="btn btn-default btn-flat">Sign out</NavLink>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-              {/*<li>
-                <a href="#" data-toggle="control-sidebar"><i className="fa fa-gears"></i></a>
-              </li>*/}
-            </ul>
-          </div>
-        </nav>
-      </header>
-    );
-  }
+							<li className="dropdown notifications-menu">
+								<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+									<i className="fa fa-bell-o"></i>
+									<span className="label label-warning">10</span>
+								</a>
+								<ul className="dropdown-menu">
+									<li className="header">You have 10 notifications</li>
+									<li>
+										<ul className="menu">
+											<li>
+												<a href="#">
+													<i className="fa fa-users text-aqua"></i> 5 new members joined today
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="footer"><a href="#">View all</a></li>
+								</ul>
+							</li>
+							<li className="dropdown tasks-menu">
+								<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+									<i className="fa fa-flag-o"></i>
+									<span className="label label-danger">9</span>
+								</a>
+								<ul className="dropdown-menu">
+									<li className="header">You have 9 tasks</li>
+									<li>
+										<ul className="menu">
+											<li>
+												<a href="#">
+													<h3>
+														Design some buttons
+														<small className="pull-right">20%</small>
+													</h3>
+													<div className="progress xs">
+														<div className="progress-bar progress-bar-aqua" style={this.progress_bar} role="progressbar"
+														     aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+															<span className="sr-only">20% Complete</span>
+														</div>
+													</div>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="footer">
+										<a href="#">View all tasks</a>
+									</li>
+								</ul>
+							</li>*/}
+							<li className="dropdown user user-menu">
+								<NavLink to="/" className="dropdown-toggle" data-toggle="dropdown">
+									<img src="/dist/img/avatar.jpg" className="user-image" alt="avatar"/>
+									<span className="hidden-xs">{this.state.user.email}</span>
+								</NavLink>
+								<ul className="dropdown-menu">
+									<li className="user-header">
+										<img src="/dist/img/avatar.jpg" className="img-circle" alt="avatar"/>
+										<p>
+											Alexander Pierce - Web Developer
+											<small>Member since Nov. 2012</small>
+										</p>
+									</li>
+									<li className="user-body">
+										<div className="row">
+											<div className="col-xs-4 text-center">
+												<NavLink to="/">Followers</NavLink>
+											</div>
+											<div className="col-xs-4 text-center">
+												<NavLink to="/">Sales</NavLink>
+											</div>
+											<div className="col-xs-4 text-center">
+												<NavLink to="/">Friends</NavLink>
+											</div>
+										</div>
+									</li>
+									<li className="user-footer">
+										<div className="pull-left">
+											<NavLink to="/profile" className="btn btn-default btn-flat">Profile</NavLink>
+										</div>
+										<div className="pull-right">
+											<NavLink to="/logout" className="btn btn-default btn-flat">Sign out</NavLink>
+										</div>
+									</li>
+								</ul>
+							</li>
+							{/*<li>
+							 <a href="#" data-toggle="control-sidebar"><i className="fa fa-gears"></i></a>
+							 </li>*/}
+						</ul>
+					</div>
+				</nav>
+			</header>
+		);
+	}
 }
 
 export default Header;
